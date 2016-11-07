13333f7133951aa013da80bc92b038f1e3240175
Daniele Sangineto <scidros@gmail.com>
2016-11-07 19:21:50 +0100

    Migliorata la UpgradeBar con l'assegnamento di monete in base al livello raggiunto Abbozzato il MainMenu con tasti funzionanti (il newgame per ora porta alla mia scena prova) ed il tasto Credits che porta all'omonima scena

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	
+	public void NewGame ()
+    {
+        SceneManager.LoadScene("Daniele_Upgrade");
+	}
+	
+	
+	public void Credits ()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+   
+} 
