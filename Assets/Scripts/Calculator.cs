d41d9bf3499a693bffb39cbbafdca2826b93a5c7
Luca Neri <lucasmodzita@gmail.com>
2016-11-07 17:10:15 +0100

    Calculator

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,34 +1,28 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
 
-public class Calculator : MonoBehaviour {
-	
-	private void Start ()
-	{
-	}
-	
-	public void CalcNewsValue(Button _button)
-	{
-		if (_button.isTrue && _button.isInteresting) 
-		{
-			return _button.GetComponent<News> ().weight = 10f;
-		}
-		else if (_button.isTrue && !_button.isInteresting) 
-		{
-			return _button.GetComponent<News> ().weight = 5f;
-		}
-		else if (!_button.isTrue) 
-		{
-			return _button.GetComponent<News> ().weight = -2f;
-		}
-	}
+public class Calculator : MonoBehaviour
+{
+
+	public void CalcNewsValue(News _button)
+    {
+        if (_button.isTrue && _button.isInteresting)
+        {
+            _button.GetComponent<News>().weight = 10;
+        }
+
+        else if (_button.isTrue && !_button.isInteresting)
+        {
+            _button.GetComponent<News>().weight = 5;
+        }
+
+        else if (!_button.isTrue)
+        {
+            _button.GetComponent<News>().weight = -2;
+        }
+    }
 
 	public void JournalValue()
 	{
-
-
-
-
+        
 	}
 }
