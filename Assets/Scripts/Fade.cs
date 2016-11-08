87f1affa4d4ce742d082af64ac50017e5a811ea0
Luca Neri <lucasmodzita@gmail.com>
2016-11-08 02:30:24 +0100

    Fade

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour
+{
+    public void FadeIn(CanvasGroup _canvasGroup)
+    {
+        StartCoroutine(FadeInCO(_canvasGroup));
+    }
+
+    public void FadeOut(CanvasGroup _canvasGroup)
+    {
+        StartCoroutine(FadeOutCO(_canvasGroup));
+    }
+
+    private IEnumerator FadeInCO(CanvasGroup _canvasGroup)
+    {
+        while (_canvasGroup.alpha < 1)
+        {
+            _canvasGroup.alpha += 0.001f;
+            yield return null;
+        }
+
+        yield break;
+    }
+
+    private IEnumerator FadeOutCO(CanvasGroup _canvasGroup)
+    {
+        while (_canvasGroup.alpha > 0)
+        {
+            _canvasGroup.alpha -= 0.001f;
+            yield return null;
+        }
+
+        yield break;
+    }
+}
