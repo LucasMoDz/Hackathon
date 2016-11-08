334be812a48b566027474f980743b3babc586c7a
Andrea Mercuri <andrea.mercuri21@gmail.com>
2016-11-08 14:05:03 +0100

    - Funzione appare botone publish dopo aver impaginato il giornale - Aggiunte tutte le 4 stanze definitive

--- Assets/Scripts/CellMultiplier.cs
+++ Assets/Scripts/CellMultiplier.cs
@@ -4,16 +4,36 @@ using System.Collections;
 public class CellMultiplier : MonoBehaviour {
 
 	public int value;
+	private GameManager refGM;
+	public bool addBool;
+	public bool subtractBool;
+
 
 	// Use this for initialization
 	void Start ()
     {
-	
+		refGM = FindObjectOfType<GameManager>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+		if (this.gameObject.transform.childCount > 0 && !addBool) {
+			addBool = true;
+			subtractBool = false;
+			Debug.Log ("NEWS ATTACHED");
+			if (refGM.newsAttachedCount < 6) {
+				refGM.newsAttachedCount++;
+			}
+		} else if(this.gameObject.transform.childCount <= 0 && !subtractBool){
+			addBool = false;
+			subtractBool = true;
+			if (refGM.newsAttachedCount > 0) {
+				refGM.newsAttachedCount--;
+			}
+
+			Debug.Log ("NEWS DETACHED");
+
+		}
 	}
 }
