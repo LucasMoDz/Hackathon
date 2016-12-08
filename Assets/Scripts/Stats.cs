3caa8b17918a5280b3b3f04e814aa4a0dec2bca1
Daniele Sangineto <scidros@gmail.com>
2016-12-08 22:28:18 +0100

    implementazione del sistema di statistiche da inspector che appaiono nella scheda del personaggio cliccato

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour {
+
+    public string name;
+    public int level = 0;
+    public float exp = 0;
+    public float research = 0;
+    public float typing = 0;
+    public float editing = 0;
+    public float network = 0;
+
+
+
+
+}
--- Assets/Scripts/StatsMenu.cs
+++ Assets/Scripts/StatsMenu.cs
@@ -7,7 +7,14 @@ public class StatsMenu : MonoBehaviour
 {
     bool active = true;
     public RectTransform panel;
+    public Text name;
     public Text level;
+    public Slider experience;
+    public Slider research;
+    public Slider typing;
+    public Slider editing;
+    public Slider network;
+
 
     void Start()
     {
@@ -28,29 +35,44 @@ public class StatsMenu : MonoBehaviour
                
                 if (hit.collider.name == "redattore" && active == true)
                 {
-                    active = false;
-                    CallPanel(hit.transform.GetComponent<Stats>().level.ToString());
+                        active = false;
+                        CallPanel(hit.transform.GetComponent<Stats>().name, 
+                        hit.transform.GetComponent<Stats>().level.ToString(), 
+                        hit.transform.GetComponent<Stats>().exp,
+                        hit.transform.GetComponent<Stats>().research,
+                        hit.transform.GetComponent<Stats>().typing,
+                        hit.transform.GetComponent<Stats>().editing,
+                        hit.transform.GetComponent<Stats>().network);
                 }
 
-
+                
             }
            
         }
 
     }
 
-    public void CallPanel(string livello)
+    public void CallPanel(string nome,  string livello, float exp, float search, float type, float edit, float net)
     {
         
-        active = true;
         panel.gameObject.SetActive(true);
+        name.text = nome;
         level.text = livello;
+        experience.value = exp / 1000;
+        research.value = search / 1000;
+        typing.value = type / 1000;
+        editing.value = edit / 1000;
+        network.value = net / 1000;
+        active = true;
     }
 
     public void ClosePanel()
     {
         panel.gameObject.SetActive(false);
+        
     }
+
+    
 
 
 }
