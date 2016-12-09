d0ee578902f06666ff1dd2b5620b4943dee41229
Luca Neri <lucasmodzita@gmail.com>
2016-12-09 15:12:26 +0100

    Coroutine button

--- Assets/Scripts/CoroutineButton.cs
+++ Assets/Scripts/CoroutineButton.cs
@@ -6,49 +6,28 @@ public class CoroutineButton : MonoBehaviour
 {
     private CreateRSSList listRSSCreated;
     private Archive archive;
-    //private Calculator refCalculator;
-    //private GameManager refGM;
-    
+
     private bool test;
-
-    //private int sizeListTrue, sizeListFalse;
     private float random_second;
-    //public int randomValue_true;
-    //public int randomValue_false;
-
-    int nRandom;
+    private int nRandom;
     
     private void Awake()
     {
         listRSSCreated = FindObjectOfType<CreateRSSList>();
         archive = FindObjectOfType<Archive>();
-        //refCalculator = FindObjectOfType<Calculator>();
-        //refGM = FindObjectOfType<GameManager>();
-        
-        random_second = Random.Range(4, 8);
-
-        //int nRandom = Random.Range(0, rssReader.channelNews.newsList.Count);
-        //this.gameObject.GetComponent<News>().titleNews = rssReader.channelNews.newsList[nRandom].title;
-
-        nRandom = Random.Range(0, listRSSCreated.maxNews);
-        this.gameObject.GetComponent<News>().titleNews = listRSSCreated.totalNewsList[nRandom].titleNews;
-
-        StartCoroutine(Spawn());
     }
-
-    /*
     
 	private void OnEnable()
 	{
 		StartCoroutine(Spawn());
 	}
-    */
 
     public IEnumerator Spawn()
     {
+        random_second = Random.Range(4, 8);
+
         while (true)
         {
-            Debug.Log("Sto aggiornando i testi dei bottoni");
             // Soltanto la prima volta
             if (!test)
             {
@@ -74,28 +53,16 @@ public class CoroutineButton : MonoBehaviour
         // 70 % scelta lista vera, 30 % scelta lista falsa
         if (randomValue <= 80)
         {
-            // Lista vera
-            //int nRandom = Random.Range(0, rssReader.channelNews.newsList.Count);
-            //this.gameObject.GetComponent<News>().titleNews = rssReader.channelNews.newsList[nRandom].title.Replace("\n", "").Replace("\r", "").Replace("\t", "");
-
             nRandom = Random.Range(0, listRSSCreated.maxNews);
             this.gameObject.GetComponent<News>().titleNews = listRSSCreated.totalNewsList[nRandom].titleNews.Replace("\n", "").Replace("\r", "").Replace("\t", "");
-            //this.gameObject.GetComponent<News>().isTrue = archive.listTrueNews[randomValue_true].isTrue;
-            //this.gameObject.GetComponent<News>().titleNews = archive.listTrueNews[randomValue_true].titleNews;
         }
         else
         {
             // Lista falsa
-            //this.gameObject.GetComponent<News>().isTrue = archive.listFalseNews[Random.Range(0, archive.listFalseNews.Count)].isTrue;
-            // this.gameObject.GetComponent<News>().isInteresting = archive.listFalseNews[randomValue_false].isInteresting;
-
             nRandom = Random.Range(0, archive.listFalseNews.Count);
             this.gameObject.GetComponent<News>().titleNews = archive.listFalseNews[nRandom].titleNews;
         }
 
         this.gameObject.GetComponentInChildren<Text>().text = this.GetComponent<News>().titleNews;
-        
-        // Assegno un peso
-        //refCalculator.CalcNewsValue(this.GetComponent<News>());
     }
 }
