a0f7a9ec767e93f22a3febe84c1014ee9b97007d
Andrea Mercuri <andrea.mercuri21@gmail.com>
2016-12-10 15:55:45 +0100

    Revert "- Creato tool per inserire gli item e il MenuShop prende in automatico Sprite, Nome e (non ancora) il prezzo"
    This reverts commit bd06383150ee5d8b64b251977195f7473909c0fd.

--- Assets/Scripts/MenuShop.cs
+++ Assets/Scripts/MenuShop.cs
@@ -1,35 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
-
-[System.Serializable]
-public struct ShopItem
-{
-    public string itemName, itemPrice;
-    public Sprite itemSprite;
-    //public Mesh itemMesh;
-}
 
 public class MenuShop : MonoBehaviour {
 
-    public ShopItem[] ShopItemContainer;
-    Button[] buttonArray;
-    Text textItem, priceItem;
     public GameObject panelShop;
     bool isInMenu;
 
 	void Start ()
     {
-        buttonArray = panelShop.GetComponentsInChildren<Button>();
 
-        for (int i = 0; i < ShopItemContainer.Length; i++)
-        {
-            buttonArray[i].image.sprite = ShopItemContainer[i].itemSprite;
-            textItem = buttonArray[i].GetComponentInChildren<Text>();
-            textItem.GetComponentInChildren<Text>().text = "dick";
-            textItem.text = ShopItemContainer[i].itemName;
-        }
     }
 	
 	void Update ()
