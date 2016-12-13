9e7a4bb87f82793b33b3ebbc1717fd317dc866bb
Luca Neri <lucasmodzita@gmail.com>
2016-12-13 23:01:09 +0100

    - SponsorLoop in progresso

---
+++ Assets/Scripts/SponsorLoop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SponsorLoop : MonoBehaviour
+{
+    public Sprite[] sponsorSprites;
+    
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(3, 6));
+            this.GetComponent<Image>().sprite = sponsorSprites[Random.Range(0, sponsorSprites.Length)];
+        }
+    }
+
+    private IEnumerator Fade(Image _currentImage)
+    {
+        while (_currentImage.color.a > 0)
+        {
+
+        }
+    }
+}
