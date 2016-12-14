6847ae0abbb123643ff66ad8a8926786fd074bfb
Luca Neri <lucasmodzita@gmail.com>
2016-12-14 03:56:10 +0100

    - Movimento pannello

---
+++ Assets/Scripts/MoveTowardsDown.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsDown : MonoBehaviour
+{
+    private bool moving;
+    internal bool moveUp;
+
+    public GameObject panel;
+    public Transform targetB;
+    public Transform targetA;
+    
+	public void StartMovementOfPanel()
+    {
+        if (!moving)
+        {
+            if (!moveUp)
+                StartCoroutine(MoveTowardsDownCO());
+            else
+                StartCoroutine(MoveTowardsUpCO());
+        }
+    }
+	
+    private IEnumerator MoveTowardsDownCO()
+    {
+        moveUp = !moveUp;
+        moving = true;
+
+        while ((panel.GetComponent<Transform>().position - targetB.position).magnitude > 5f)
+        {
+            panel.GetComponent<RectTransform>().position -= new Vector3(0, 12);
+            yield return new WaitForEndOfFrame();
+        }
+
+        moving = false;
+        yield break;
+    }
+
+    private IEnumerator MoveTowardsUpCO()
+    {
+        moveUp = !moveUp;
+        moving = true;
+
+        while ((panel.GetComponent<Transform>().position - targetA.position).magnitude > 5f)
+        {
+            panel.GetComponent<RectTransform>().position += new Vector3(0, 12);
+            yield return new WaitForEndOfFrame();
+        }
+
+        moving = false;
+        yield break;
+    }
+}
