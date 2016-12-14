acfe3e81b23068dfbaaaa46cc2f5f72e58e7c2c5
Daniele Sangineto <scidros@gmail.com>
2016-12-14 02:31:07 +0100

    prova

--- Assets/Scripts/MenuEditors.cs
+++ Assets/Scripts/MenuEditors.cs
@@ -33,7 +33,7 @@ public class MenuEditors : MonoBehaviour
         for (int i = 0; i < editorContainers.Length; i++)
         {
             Stats mioRedattore = editorContainers[i].editorButton.GetComponent<Selector>().redattore.GetComponent<Stats>();
-            editorContainers[i].editorButton.transform.GetChild(0).GetComponent<Image>().sprite = mioRedattore.image;
+            editorContainers[i].editorButton.transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = mioRedattore.image;
             editorContainers[i].editorButton.transform.GetChild(1).GetComponent<Text>().text = mioRedattore.nameof;
             editorContainers[i].editorButton.transform.GetChild(2).GetComponent<Text>().text = "Lv. " + mioRedattore.level;
         }
