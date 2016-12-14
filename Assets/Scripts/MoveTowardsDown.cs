bd2799e4c79885358993a8a1f93c18bb68696451
Luca Neri <lucasmodzita@gmail.com>
2016-12-14 04:05:08 +0100

    - Fix raycast

--- Assets/Scripts/MoveTowardsDown.cs
+++ Assets/Scripts/MoveTowardsDown.cs
@@ -23,9 +23,10 @@ public class MoveTowardsDown : MonoBehaviour
 	
     private IEnumerator MoveTowardsDownCO()
     {
+        panel.SetActive(true);
         moveUp = !moveUp;
         moving = true;
-
+        
         while ((panel.GetComponent<Transform>().position - targetB.position).magnitude > 5f)
         {
             panel.GetComponent<RectTransform>().position -= new Vector3(0, 12);
@@ -38,6 +39,7 @@ public class MoveTowardsDown : MonoBehaviour
 
     private IEnumerator MoveTowardsUpCO()
     {
+        panel.SetActive(true);
         moveUp = !moveUp;
         moving = true;
 
