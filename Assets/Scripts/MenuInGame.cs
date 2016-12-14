2d6399e372c872e9b7da6a14864ad2aba9a8c0eb
Daniele Sangineto <scidros@gmail.com>
2016-12-14 20:57:22 +0100

    - Aggiornato il Prefab del CanvasPause con il nuovo pulsante Credits, funzionante sulla Game_Scena e che richiama il Panel Credits che si accende e si spegne seguendo la logica dei metodi dichiarati nello script MenuInGame

--- Assets/Scripts/MenuInGame.cs
+++ Assets/Scripts/MenuInGame.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuInGame : MonoBehaviour {
 
-    public GameObject panelPause, pauseButton;
+    public GameObject panelPause, pauseButton, panelCredits;
     SoundController refSound;
 
     private void Start()
@@ -21,6 +21,24 @@ public class MenuInGame : MonoBehaviour {
         Time.timeScale = 0;
         panelPause.SetActive(true);
         pauseButton.SetActive(false);
+    }
+
+    // Apre i Credits
+    public void CreditsOn()
+    {
+        refSound.audioSource.Play();
+        Time.timeScale = 0;
+        panelCredits.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+
+    // Chiude i Credits
+    public void CreditsOff()
+    {
+        refSound.audioSource.Play();
+        Time.timeScale = 1;
+        panelCredits.SetActive(false);
+        pauseButton.SetActive(true);
     }
 
     // Riprende il gioco
