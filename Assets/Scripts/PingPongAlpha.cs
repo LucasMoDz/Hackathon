9e3bdaa4b635f2c712af7171a0d7dbe9fedab125
Luca Neri <lucasmodzita@gmail.com>
2016-12-14 23:17:01 +0100

    - Main Menu pronto

---
+++ Assets/Scripts/PingPongAlpha.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PingPongAlpha : MonoBehaviour
+{
+    private float myAlpha = 1;
+
+    private IEnumerator Start()
+    {
+        while(true)
+        {
+            myAlpha = Mathf.PingPong(Time.time, 1);
+            this.GetComponent<Text>().color = new Color(0, 0, 0 , myAlpha);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
