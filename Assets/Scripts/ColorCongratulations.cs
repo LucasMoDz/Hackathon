cb9d5db9f82716c8616107de226a16d09868797f
dbga_36 <dbga_36@dbga.local>
2016-12-14 16:00:28 +0100

    - Riepilogo

---
+++ Assets/Scripts/ColorCongratulations.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ColorCongratulations : MonoBehaviour
+{
+	private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.5f));
+            this.GetComponent<Text>().color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/RiepilogoManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiepilogoManager : MonoBehaviour
+{
+	void Start ()
+    {
+	
+	}
+
+	void Update ()
+    {
+	
+	}
+}
