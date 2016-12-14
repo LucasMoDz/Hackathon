9a36adf14a9dc9521ed4eadfb070855af93fb984
Daniele Sangineto <scidros@gmail.com>
2016-12-14 23:51:51 +0100

    - aggiunta la scena Logo con logica che dopo 3 secondi carica la Game_Scena

---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Logo : MonoBehaviour {
+
+	
+	IEnumerator Start ()
+    {
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("Game_Scena");
+    }
+	
+    
+	
+	void Update () {
+	
+	}
+}
